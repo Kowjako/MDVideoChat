596eb5f7f45ff381661f0daa73bcda3f40bc71a8
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-03-30 16:30:21 +0200

    Add start P2P video realization & datagram segmentation

---
+++ videochat udp/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace videochat_udp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myVideoPictureBox = new System.Windows.Forms.PictureBox();
+            this.friendVideoPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.localLbl = new System.Windows.Forms.Label();
+            this.remoteLbl = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.myVideoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.friendVideoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myVideoPictureBox
+            // 
+            this.myVideoPictureBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.myVideoPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.myVideoPictureBox.Name = "myVideoPictureBox";
+            this.myVideoPictureBox.Size = new System.Drawing.Size(454, 346);
+            this.myVideoPictureBox.TabIndex = 0;
+            this.myVideoPictureBox.TabStop = false;
+            // 
+            // friendVideoPictureBox
+            // 
+            this.friendVideoPictureBox.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.friendVideoPictureBox.Location = new System.Drawing.Point(500, 12);
+            this.friendVideoPictureBox.Name = "friendVideoPictureBox";
+            this.friendVideoPictureBox.Size = new System.Drawing.Size(454, 346);
+            this.friendVideoPictureBox.TabIndex = 1;
+            this.friendVideoPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Connect To";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(798, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(879, 374);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 377);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(330, 379);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(520, 379);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // localLbl
+            // 
+            this.localLbl.AutoSize = true;
+            this.localLbl.Location = new System.Drawing.Point(272, 382);
+            this.localLbl.Name = "localLbl";
+            this.localLbl.Size = new System.Drawing.Size(52, 13);
+            this.localLbl.TabIndex = 8;
+            this.localLbl.Text = "LocalPort";
+            // 
+            // remoteLbl
+            // 
+            this.remoteLbl.AutoSize = true;
+            this.remoteLbl.Location = new System.Drawing.Point(451, 382);
+            this.remoteLbl.Name = "remoteLbl";
+            this.remoteLbl.Size = new System.Drawing.Size(63, 13);
+            this.remoteLbl.TabIndex = 9;
+            this.remoteLbl.Text = "RemotePort";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(648, 376);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(966, 416);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.remoteLbl);
+            this.Controls.Add(this.localLbl);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.friendVideoPictureBox);
+            this.Controls.Add(this.myVideoPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.myVideoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.friendVideoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox myVideoPictureBox;
+        private System.Windows.Forms.PictureBox friendVideoPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label localLbl;
+        private System.Windows.Forms.Label remoteLbl;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ videochat udp/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Imaging.Filters;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace videochat_udp
+{
+    public partial class Form1 : Form
+    {
+        [Serializable]
+        public class FileDetails
+        {
+            public string FILESIZE = "";
+        }
+
+        private Bitmap actualFrame;
+
+        private Mirror filter = new Mirror(false, true);
+        private ResizeNearestNeighbor filterSize = new ResizeNearestNeighbor(454, 346);
+
+        private FileDetails fileDet = new FileDetails();
+        private FileDetails fileDet2 = new FileDetails();
+        private Thread receiveThread, sendThread;
+
+        private byte[] sendedBytes;
+        private byte[] receivedBytes;
+
+        private Random random = new Random();
+        EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+        private MemoryStream memStr = null;
+
+        public FilterInfoCollection recordingDevices = null;
+        public VideoCaptureDevice videoSource = null;
+        static IPAddress remoteAddress; // хост для отправки данных
+        static int remotePort; // порт для отправки данных
+        int localPort; // локальный порт для прослушивания входящих подключений
+
+        //UdpClient sender = null;
+        static Socket receiveSocket;
+        IPEndPoint endPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                //remoteAddress = IPAddress.Parse("127.0.0.1");
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            recordingDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo f in recordingDevices)
+            {
+                comboBox1.Items.Add(f.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+            videoSource = new VideoCaptureDevice();
+            /* Event which will be invoked when new frame is arrived */
+
+            
+        }
+
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (actualFrame != null) actualFrame.Dispose();
+            actualFrame = (Bitmap)eventArgs.Frame.Clone();
+            filter.ApplyInPlace(actualFrame);
+            if (myVideoPictureBox.Image != null) this.Invoke(new MethodInvoker(delegate () { myVideoPictureBox.Image.Dispose(); }));
+            myVideoPictureBox.Image = filterSize.Apply(actualFrame);
+            /*Every new frame will be sended to another user */
+            if(remoteAddress.ToString() != "0.0.0.0" && remoteAddress != null)
+            {
+               // MessageBox.Show("Start Frame sended");
+                SendVideo(endPoint, actualFrame);
+               
+            }
+        }
+        UdpClient client;
+        private void ReceiveMessage()
+        {
+                client = new UdpClient(localPort);
+                while (true)
+                {
+                    if(friendVideoPictureBox.Image!= null) friendVideoPictureBox.Image.Dispose();
+                    GetFile();
+                }
+        }
+
+        public void SendVideo(IPEndPoint endPoint, Image img)
+        {
+            ImageConverter _imageConverter = new ImageConverter();
+            Socket listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            Byte[] arrImage = ms.ToArray();
+            //MessageBox.Show("Rozmiar arrImage = ", arrImage.Length.ToString());
+            if (arrImage.Length < 60000)
+            {
+                listeningSocket.SendTo(arrImage, endPoint);
+            }
+            else
+            {
+
+                int parts = arrImage.Length / 60000;
+                int fixedLength = arrImage.Length;
+                int drift = 0;
+                FileStream fs = new FileStream("temp.jpeg", FileMode.Create, FileAccess.ReadWrite, FileShare.ReadWrite);
+                for (int i = 0; i < parts + 1; i++)
+                {
+                    if (fixedLength - 60000 >= 0)
+                    {
+                        listeningSocket.SendTo(arrImage, drift, 60000, SocketFlags.None, endPoint);
+                        fixedLength -= 60000;
+                        drift += 60000;
+                    }
+                    else
+                    {
+                        listeningSocket.SendTo(arrImage, drift, fixedLength, SocketFlags.None, endPoint);
+                    }
+                }
+               // MessageBox.Show("Completed!");
+            }
+        }
+
+        public static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                return stream.ToArray();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            remoteAddress = IPAddress.Parse(textBox1.Text);
+            remotePort = Convert.ToInt32(textBox3.Text);
+            localPort = Convert.ToInt32(textBox2.Text);
+
+            endPoint = new IPEndPoint(remoteAddress, remotePort);
+            
+
+            if (remoteAddress.ToString() == "0.0.0.0")
+            {
+               // //receiveSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                //IPEndPoint localIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), localPort);
+               // receiveSocket.Bind(localIP);
+
+                receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+            }
+
+            if (remoteAddress.ToString() != "0.0.0.0" && remoteAddress != null)
+            {
+                //sendThread = new Thread(new ThreadStart(SendMessage));
+                //sendThread.Start();
+            }
+            videoSource = new VideoCaptureDevice(recordingDevices[comboBox1.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(VideoSource_NewFrame);
+            videoSource.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            videoSource.Stop();
+        }
+         
+        public void GetFile()
+        {
+            try
+            {
+                
+                IPEndPoint endP = new IPEndPoint(IPAddress.Any, 0);
+                while(true)
+                {
+                    MemoryStream ms = new MemoryStream();
+                    for (int i=0;i<3;i++)
+                    {
+                        Byte[] arrImage = client.Receive(ref endP);
+                        ms.Write(arrImage, 0, arrImage.Length);
+                        if (arrImage.Length < 60000) break;
+                    }
+                    Image rcvdImage = (Bitmap)((new ImageConverter()).ConvertFrom(ms.ToArray()));
+                    friendVideoPictureBox.Image = filterSize.Apply((Bitmap)rcvdImage);
+                  //  MessageBox.Show("FILE GETTED!");
+                }
+            }
+            catch (Exception eR)
+            {
+                MessageBox.Show("GET FILE: " + eR.ToString() + "STACK TRACE: " + eR.StackTrace);
+            }
+        } 
+    }
+}
+
